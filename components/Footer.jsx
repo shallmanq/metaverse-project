87a086f758567b2695a71798d7ac477fcf468761
Shallman Quashie <shallmanq@gmail.com>
2024-08-26 23:40:43 +0100

    footer file update

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -18,7 +18,7 @@ const Footer = () => (
     <div className={`${styles.innerWidth} mx-auto flex flex-col gap-8`}>
       <div className="flex items-center justify-between flex-wrap gap-5">
         <h4 className="font-bold md:text-[64px] text-[44px] text-white">
-          Enter New Realites
+          Enter New Realities
         </h4>
         <button
           type="button"
